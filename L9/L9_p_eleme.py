1732582f534dc73b3d5ad21a993dc59b1b6149df
PlusLee <pluslee@aliyun.com>
2019-08-18 00:47:38 +0800

    debug
    发现自己原来写的有bug

--- L9/L9_p_eleme.py
+++ L9/L9_p_eleme.py
@@ -10,16 +10,16 @@ def cookies_read():
     cookies_text = open('cookies_ele.txt','r')
     cookies_dict = json.loads(cookies_text.read())
     cookies = requests.utils.cookiejar_from_dict(cookies_dict)
-
+    return cookies
 #   登陆函数---包含了短信验证码、token获取，未做图形验证
 def log_in():
     url_sendcode = ' https://h5.ele.me/restapi/eus/login/mobile_send_code'
-    url_get_token  = 'https://h5.ele.me/restapi/eus/login'
+    # url_get_token  = 'https://h5.ele.me/restapi/eus/login'
     usr_cellphone = input('请输入手机号码：')
     data_sendcode = {'captcha_hash': "",
             'captcha_value': "",
             'mobile': usr_cellphone,
-            'scf': "ms"}
+            'scf': ""}
     #   获取校验token
     response = session.post(url_sendcode,headers=headers,data=data_sendcode)
     response_token = response.json()['validate_token']
@@ -31,7 +31,7 @@ def log_in():
                 'validate_token': response_token}
     login = session.post(url_login,headers=headers,data=data_login)
     #   写入cookies
-    cookies_dict = requests.utils.cookiejar_from_dict(login.cookies)
+    cookies_dict = requests.utils.dict_from_cookiejar(login.cookies)
     cookies_str  = json.dumps(cookies_dict)
     with open('cookies_ele.txt','w') as wf:
         wf.write(cookies_str)
@@ -55,25 +55,29 @@ def location():
         i += 1
     chose_num = int(input('请选择您需要的位置所对应的序号：'))
     location_final = location_json[chose_num]
+    print(location_final)
     return location_final
 
 #   获取餐馆列表
-def resturant():
+def resturant(session):
     url_rst = 'https://www.ele.me/restapi/shopping/restaurants?'
     location_detail = location()
-    latitude = str(location_detail['latitude'])
-    longitude = str(location_detail['longitude'])
+    geohash = location_detail['geohash']
+    latitude = location_detail['latitude']
+    longitude = location_detail['longitude']
     params = {'extras[]': 'activities',
-            'geohash': 'wtmkrd5xd21j',
-            'latitude': latitude,
+            'geohash': geohash,
+            'latitude': str(latitude),
             'limit': '24',
-            'longitude': longitude,
+            'longitude': str(longitude),
             'offset': '0',
             'terminal': 'web'}
-    res_json = requests.get(url_rst,headers=headers,params=params).json()
+    res = session.get(url_rst,headers=headers,params=params)
+    print(res.status_code)
+    res_json = res.json()
     resturant_list = []
     for resturant in res_json:
-        resturant_info = ['名称：'+resturant['name'],'评分：'+resturant['rating'],'简介：'+resturant['promotion_info'],'店铺链接：'+resturant['scheme']]
+        resturant_info = ['名称：'+resturant['name'],'评分：'+str(resturant['rating']),'简介：'+resturant['promotion_info'],'店铺链接：'+resturant['scheme']]
         resturant_list.append(resturant_info)
     return resturant_list
 
@@ -83,7 +87,9 @@ except  FileNotFoundError:
     log_in()
     session.cookies = cookies_read()
 
-rst_list = resturant()
+rst_list = resturant(session)
 for i in rst_list:
+    # print(i)
     for k in i:
-        print(k,'\n\n')
+        print(k)
+    print('\n\n')
